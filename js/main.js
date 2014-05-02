a7bc11fceb440cac6f5c527cd65485d0954c2208
John van de Water <john.vd.water85@gmail.com>
2014-05-02 13:43:58 +0200

    Add some responsiveness

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// ;(function() {
+//
+//   if($('#menu-affix').outerHeight() < $(window).outerHeight()) {
+//
+//     $('#menu-affix').affix({
+//       offset: {
+//         top: 0,
+//         bottom: 200
+//       }
+//     })
+//
+//   }
+//
+// })()

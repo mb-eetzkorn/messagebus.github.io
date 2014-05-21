e919c1978b595c370fd9c0034f0640d4b3eab6cb
John van de Water <john.vd.water85@gmail.com>
2014-05-21 14:07:13 +0200

    Fix affix issue

--- js/main.js
+++ js/main.js
@@ -7,7 +7,12 @@
         top: 10,
         bottom: 150
       }
-    })
+    });
+
+    $(window).on('scroll', function(e) {
+      if (($('body').scrollTop() + $(window).height()) - $('body').height() < -150)
+        $('.affix-bottom').removeClass('affix-bottom');
+    });
 
   }
 
